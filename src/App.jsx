b8ff2763128e7a446ec01d9998717ba913503052
unknown <shohidulislamsifat2003@gmail.com>
2024-10-22 07:30:07 +0600

    add all templete in frist

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Blogs from './assets/Component/Blogs/Blogs'
+import Header from './assets/Component/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+<Header></Header>
+<Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
